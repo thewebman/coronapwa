f4ebf91da2c11221899d00642f8de41db9507447
thewebman <34786376+thewebman@users.noreply.github.com>
2020-09-14 03:53:51 -0400

    Add files via upload

---
+++ precache-manifest.ca2edeef1575680f2a90e44096de210f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21660dd078789aa2efaf94d0e1d699cb",
+    "url": "/coronapwa/index.html"
+  },
+  {
+    "revision": "3a926586d3a69a944d20",
+    "url": "/coronapwa/static/css/main.3d7bb7c8.chunk.css"
+  },
+  {
+    "revision": "7558f8d6e95f29f808f4",
+    "url": "/coronapwa/static/js/2.d1613e94.chunk.js"
+  },
+  {
+    "revision": "c46ade285a7bd9e73d4de71c9509bd70",
+    "url": "/coronapwa/static/js/2.d1613e94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a926586d3a69a944d20",
+    "url": "/coronapwa/static/js/main.fc0c67a6.chunk.js"
+  },
+  {
+    "revision": "68270ed8a4628e36a1d3",
+    "url": "/coronapwa/static/js/runtime-main.5f87c58f.js"
+  }
+]);
